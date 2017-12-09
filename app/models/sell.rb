2b67b5b5a43d79f965b61dbaee87c85a8e7e542c
rafaelBadan <rafael.badan@gmail.com>
2017-12-09 16:34:41 -0200

    implements sales relations

--- app/models/sell.rb
+++ app/models/sell.rb
@@ -1,7 +1,6 @@
 class Sell < ApplicationRecord
   include Fae::BaseModelConcern
-  enum status: { finished: 0, canceled: 1 }
-  
+  enum status: { finalizada: 0, cancelada: 1 }
   validates :client, presence: true
 
   belongs_to :discount
@@ -9,9 +8,11 @@ class Sell < ApplicationRecord
 
   has_many :sell_products
   has_many :products, through: :sell_products
-  
+
   has_many :sell_services
   has_many :services, through: :sell_services
+
+  before_save :set_total
 
   def fae_display_field
     id
@@ -20,5 +21,19 @@ class Sell < ApplicationRecord
   def self.for_fae_index
     order(:id)
   end
-  
+
+  private
+
+  def set_total
+    total = 0
+    self.products.each {|p| total += p.price }
+    self.services.each {|s| total += s.price }
+
+    if self.discount.present?
+      total = total - self.discount.value
+    end
+
+    total = (total >= 0)? total : 0
+    self.total = total
+  end
 end
